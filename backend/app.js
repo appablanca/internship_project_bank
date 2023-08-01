bcd9e9de68ac2ecc430a7faa1016e6c6f89f9e4a
appablanca <feyzieren@gmail.com>
2023-08-01 10:18:07 +0300

    day16 start

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const MONGODB_URI ="mongodb+srv://feyzieren:f8wjV80YPIP9vbIg@cluster0.jxeprcj.mongodb.net/bank?retryWrites=true&w=majority";
+
+
+
+
+
+listen(300);
